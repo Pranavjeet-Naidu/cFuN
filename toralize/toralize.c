6c032e9df1bb21ed363ad06d0a048fc0d89bd278
Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>
2024-12-01 15:21:33 +0530

    initizalizing the repo

---
+++ toralize/toralize.c
@@ -0,0 +1,23 @@
+# include "toralize.h"
+
+
+
+
+int main(int argc,char *argv[])
+{
+    char*host;
+    int port;
+
+    int (argc < 3)
+    {
+        //fprintf is like printf but takes an additional argument : where to send the output  
+        fprintf(stderr,"Usage : %s <host> <port>\n",argv[0]);
+
+        return -1;
+        
+    }
+    host = argv[1];
+    // argv[2] is a number in string format , we need to convert it to an actual number 
+    port = atoi(argv[2]);
+
+}
---
+++ toralize/toralize.h
@@ -0,0 +1,11 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+//to deal with the strings 
+# include <unistd.h>
+// used to access the POSIX API , includes functionality for system calls and standard I/O operations 
+
+// used the default values here  
+#define PROXY       "127.0.0.1"
+#define PROXYPORT   9050
+ 
